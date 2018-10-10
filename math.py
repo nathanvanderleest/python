cd24ef7d145777dcf373d9f4b364f4e7fccd1e38
nathanvanderleest <nathanvanderleest1@gmail.com>
2018-10-10 15:18:13 -0500

    adding math.py

---
+++ math.py
@@ -0,0 +1,22 @@
+
+# Numbers
+
+# In python shell use: CTRL + p (On Mac)
+#   to get the previous command
+
+print("2000 - 100 + 50 =", 2000 - 100 + 50);
+print("6 * 3 =", 6 * 3);
+print("19 / 4 =", 19 / 4);
+print("For integer division:");
+print("19 // 4 =", 19 // 4);
+print("107 // 4 =", 107 // 4);
+print("107 % 4 =", 107 % 4);
+
+print("\n");
+
+name = "Larry";
+print(name);
+print("Hi, "+name);
+
+name = input("Whats your name?");
+print("Hi",name);
