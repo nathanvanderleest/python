9e712be811fd817432f36966e88ff1b566a7ac9a
nathanvanderleest <nathanvanderleest1@gmail.com>
2018-10-10 15:18:53 -0500

    adding strings.py

---
+++ strings.py
@@ -0,0 +1,41 @@
+
+# Game Over - Version 2
+# Demonstrates the use of quotes in strings
+
+# Found this on the internet
+import os;
+os.system('say "nice."');
+print("\a\a\a");
+
+import sys;
+sys.stdout.write('\a');
+sys.stdout.flush();
+
+
+print("Program 'Game over' 2.0");
+print("Same", "message", "as before");
+
+print("Just",
+      "a bit",
+      "bigger");
+
+print("Here", end=" ");
+
+print("it is...");
+
+print(
+    """
+ ______        ___
+/  ___|       /   |
+| |          / __ |
+| |   _     / /__ |
+| |__| |   / /    |
+ \_____|  /_/     |
+    """
+);
+print("Pie" * 10);
+
+input("\n\nPress the enter key to exit.");
+
+
+
