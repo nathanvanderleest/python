f0779922187e3b7b286945ad3ea7e1cbbfff87e3
nathanvanderleest <nathanvanderleest1@gmail.com>
2018-10-10 23:30:32 -0500

    adding inOperatorLen file

---
+++ inOperatorLen.py
@@ -0,0 +1,17 @@
+
+# Message Analyzer
+# Demonstrates the len() function and the in operator
+
+message = input("Enter a message: ")
+
+print("\nThe length:", len(message))
+
+print("\nThe most common letter in the English language, 'e',")
+if "e" in message:
+    print("is in your message.")
+else:
+    print("is not in your message.")
+
+
+
+
