86540232b8d08e6d8d070d46739da67ec3b5e363
nathanvanderleest <nathanvanderleest1@gmail.com>
2018-10-10 23:28:21 -0500

    adding breakContinue file

---
+++ breakContinue.py
@@ -0,0 +1,16 @@
+
+# break
+# continue
+
+count = 0
+
+while True:
+    count += 1
+    # end loop if count greater than 10
+    if count > 10:
+        break
+    # skip 5
+    if count == 5:
+        continue
+    print(count)
+
